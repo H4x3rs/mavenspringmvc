9ab22b90f9a3266c8fe6d57d6e9636fb9edaddfd
ren <18435175817@163.com>
2017-10-19 18:41:12 +0800

    third commit

--- src/main/java/com/example/controller/UserController.java
+++ src/main/java/com/example/controller/UserController.java
@@ -1,4 +1,30 @@
 package com.example.controller;
 
+import com.example.dao.UserDao;
+import com.example.model.User;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.List;
+
+@Controller
+@RequestMapping("/user")
 public class UserController {
+    private UserDao userDao;
+
+    public UserDao getUserDao() {
+        return userDao;
+    }
+
+    public void setUserDao(UserDao userDao){
+        this.userDao=userDao;
+    }
+
+    @RequestMapping("/getAll")
+    public String getall(HttpServletRequest request){
+        List<User> userList = userDao.queryAll();
+        request.setAttribute("userlist",userList);
+        return "getAll";
+    }
 }
